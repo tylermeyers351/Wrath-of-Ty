c5cad3b3208164f2c80ce42e8593a654f9c5c598
tylermeyers351 <tylermeyers351@gmail.com>
2025-06-16 17:30:35 -0700

    Attack data and animation start.

--- Assets/Scripts/Statemachines/Player/PlayerAttackingState.cs
+++ Assets/Scripts/Statemachines/Player/PlayerAttackingState.cs
@@ -2,24 +2,59 @@ using UnityEngine;
 
 public class PlayerAttackingState : PlayerBaseState
 {
-    public PlayerAttackingState(PlayerStateMachine stateMachine) : base(stateMachine)
+    private float previousFrameTime;
+
+    private Attack currentAttack;
+
+    public PlayerAttackingState(PlayerStateMachine stateMachine, int attackIndex) : base(stateMachine)
     {
-        
+        currentAttack = stateMachine.Attacks[attackIndex];
     }
 
     public override void Enter()
     {
-        
+        stateMachine.Animator.CrossFadeInFixedTime(currentAttack.AnimationName, 0.1f);
     }
 
     public override void Tick(float deltaTime)
     {
-        
+        float normalizedTime = GetNormalizedTime();
+        if (normalizedTime > previousFrameTime && normalizedTime < 1f)
+        {
+            if (stateMachine.InputReader.IsAttacking)
+            {
+                // TryComboAttack(normalizedTime);
+            }
+        }
+        else
+        {
+            //go back to free look state
+        }
+        previousFrameTime = normalizedTime;
     }
 
     public override void Exit()
     {
-        
+
+    }
+
+    private float GetNormalizedTime()
+    {
+        AnimatorStateInfo currentInfo = stateMachine.Animator.GetCurrentAnimatorStateInfo(0);
+        AnimatorStateInfo nextInfo = stateMachine.Animator.GetNextAnimatorStateInfo(0);
+
+        if (stateMachine.Animator.IsInTransition(0) && nextInfo.IsTag("Attack"))
+        {
+            return nextInfo.normalizedTime;
+        }
+        else if (!stateMachine.Animator.IsInTransition(0) && currentInfo.IsTag("Attack"))
+        {
+            return currentInfo.normalizedTime;
+        }
+        else
+        {
+            return 0f;
+        }
     }
 
 }
