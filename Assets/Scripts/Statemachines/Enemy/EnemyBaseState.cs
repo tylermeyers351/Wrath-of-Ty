c4db5e4931ca4eea61ad89d76e8b59b8434330fe
tylermeyers351 <tylermeyers351@gmail.com>
2025-06-23 11:55:36 -0700

    Enemy states created.

---
+++ Assets/Scripts/Statemachines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected EnemyStateMachine stateMachine;
+
+    public EnemyBaseState(EnemyStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+}
---
+++ Assets/Scripts/Statemachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int EnemyAnimatorHash = Animator.StringToHash("EnemyBlendTree");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+
+    private const float AnimatorDampTime = 0.1f;
+    private const float CrossFadeDuration = 0.1f;
+
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(EnemyAnimatorHash, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        stateMachine.Animator.SetFloat(SpeedHash, 0f, AnimatorDampTime, deltaTime);
+    }
+    
+    public override void Exit() { }
+
+}
